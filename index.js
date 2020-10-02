70229e65aa7980cf370bb8c9b0113aa39a8b8772
Chinh Nguyen <chinhht09@gmail.com>
2020-10-02 23:16:26 +0700

    update error.txt

--- index.js
+++ index.js
@@ -1,46 +1,34 @@
 const express = require("express"); // Required download Express
 const app = express();
 const mongoose = require("mongoose");
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 
-const config = require('./config/key');
+const config = require("./config/key");
 
-const { User } = require('./models/user');
+const { User } = require("./models/user");
 
 mongoose
-    .connect(
-        config.mongoURI,
-        { useNewUrlParser: true }
-    )
-    .then(() => console.log("DB connected"))
-    .catch((err) => console.error(err));
+  .connect(config.mongoURI, { useNewUrlParser: true })
+  .then(() => console.log("DB connected"))
+  .catch((err) => console.error(err));
 
-app.use(bodyParser.urlencoded({ extended:true }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
+app.get("/", (req, res) => {
+  res.json({ "hello ~": "Hi ~~I love you" });
+});
 
-app.get('/',(req,res) => {
-    res.json({"hello ~":"Hi ~~I love you"})
-})
-
-
-
-
-
-app.post('/api/users/register',(req,res) => {
-        const user = new User(req.body)
-        user.save((err,userData) => {
-            if(err) return res.json({success: false, err})
-            return res.status(200).json({
-                success:true
-            })
-        })  
-})
-
-
-
-
+app.post("/api/users/register", (req, res) => {
+  const user = new User(req.body);
+  user.save((err, userData) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+      success: true,
+    });
+  });
+});
 
 app.listen(5000);
